d15aa6945216971f9c08ca89b27df684993e1bd9
malwian <maliwan@stech.ac.th>
2017-04-04 16:02:41 +0700

    Save2

--- app/src/main/java/com/example/maliwan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/maliwan/myapplication/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.maliwan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit การประการตัวแปร
+    private EditText userEditText,PasswordEditText;
+    private TextView textView;
+    private Button button;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    //Initial View คือการผูกตัวแปร กับ Viewที่อยู่ที่ Activity
+        initialView();
+
+    }// Main Method
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        PasswordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+
+
     }
-}
+
+}   // Main Class นี้คือ คลาสหลัก
